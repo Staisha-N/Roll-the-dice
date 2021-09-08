6165c027a7abb54388f04cd644c0b9d36acd3ef9
Staisha-N <90223900+Staisha-N@users.noreply.github.com>
2021-09-07 20:26:30 -0400

    Add files via upload

---
+++ Jeu_de_des_S_Neville/Form1.Designer.vb
@@ -0,0 +1,126 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblInvite = New System.Windows.Forms.Label()
+        Me.txtMise = New System.Windows.Forms.TextBox()
+        Me.btnJeterLesDes = New System.Windows.Forms.Button()
+        Me.lblMessageResultat = New System.Windows.Forms.Label()
+        Me.lblPoints = New System.Windows.Forms.Label()
+        Me.picDeUn = New System.Windows.Forms.PictureBox()
+        Me.picDeDeux = New System.Windows.Forms.PictureBox()
+        CType(Me.picDeUn, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picDeDeux, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lblInvite
+        '
+        Me.lblInvite.AutoSize = True
+        Me.lblInvite.Location = New System.Drawing.Point(23, 40)
+        Me.lblInvite.Name = "lblInvite"
+        Me.lblInvite.Size = New System.Drawing.Size(110, 20)
+        Me.lblInvite.TabIndex = 0
+        Me.lblInvite.Text = "Inscrire la mise:"
+        '
+        'txtMise
+        '
+        Me.txtMise.Location = New System.Drawing.Point(139, 37)
+        Me.txtMise.Name = "txtMise"
+        Me.txtMise.Size = New System.Drawing.Size(115, 27)
+        Me.txtMise.TabIndex = 1
+        '
+        'btnJeterLesDes
+        '
+        Me.btnJeterLesDes.Location = New System.Drawing.Point(297, 36)
+        Me.btnJeterLesDes.Name = "btnJeterLesDes"
+        Me.btnJeterLesDes.Size = New System.Drawing.Size(168, 28)
+        Me.btnJeterLesDes.TabIndex = 2
+        Me.btnJeterLesDes.Text = "Jeter les dés"
+        Me.btnJeterLesDes.UseVisualStyleBackColor = True
+        '
+        'lblMessageResultat
+        '
+        Me.lblMessageResultat.AutoSize = True
+        Me.lblMessageResultat.Location = New System.Drawing.Point(23, 289)
+        Me.lblMessageResultat.Name = "lblMessageResultat"
+        Me.lblMessageResultat.Size = New System.Drawing.Size(218, 20)
+        Me.lblMessageResultat.TabIndex = 3
+        Me.lblMessageResultat.Text = "Le total des points accumulés ="
+        '
+        'lblPoints
+        '
+        Me.lblPoints.AutoSize = True
+        Me.lblPoints.Location = New System.Drawing.Point(247, 289)
+        Me.lblPoints.Name = "lblPoints"
+        Me.lblPoints.Size = New System.Drawing.Size(41, 20)
+        Me.lblPoints.TabIndex = 4
+        Me.lblPoints.Text = "1000"
+        '
+        'picDeUn
+        '
+        Me.picDeUn.InitialImage = Nothing
+        Me.picDeUn.Location = New System.Drawing.Point(23, 86)
+        Me.picDeUn.Name = "picDeUn"
+        Me.picDeUn.Size = New System.Drawing.Size(165, 144)
+        Me.picDeUn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.picDeUn.TabIndex = 5
+        Me.picDeUn.TabStop = False
+        '
+        'picDeDeux
+        '
+        Me.picDeDeux.Location = New System.Drawing.Point(281, 86)
+        Me.picDeDeux.Name = "picDeDeux"
+        Me.picDeDeux.Size = New System.Drawing.Size(170, 144)
+        Me.picDeDeux.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.picDeDeux.TabIndex = 6
+        Me.picDeDeux.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(571, 340)
+        Me.Controls.Add(Me.picDeDeux)
+        Me.Controls.Add(Me.picDeUn)
+        Me.Controls.Add(Me.lblPoints)
+        Me.Controls.Add(Me.lblMessageResultat)
+        Me.Controls.Add(Me.btnJeterLesDes)
+        Me.Controls.Add(Me.txtMise)
+        Me.Controls.Add(Me.lblInvite)
+        Me.Name = "Form1"
+        Me.Text = "Jeu de dés (S. Neville)"
+        CType(Me.picDeUn, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picDeDeux, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblInvite As Label
+    Friend WithEvents txtMise As TextBox
+    Friend WithEvents btnJeterLesDes As Button
+    Friend WithEvents lblMessageResultat As Label
+    Friend WithEvents lblPoints As Label
+    Friend WithEvents picDeUn As PictureBox
+    Friend WithEvents picDeDeux As PictureBox
+End Class
---
+++ Jeu_de_des_S_Neville/Form1.vb
@@ -0,0 +1,112 @@
+﻿Public Class Form1
+    '******************************************************************************************************
+    '* Travail : Jeu de dés
+    '* Programmeuse : Staisha Neville
+    '* Date : Avril 2021
+    '* But : Utilisation de sous-programmes et de procédures fonction
+    '******************************************************************************************************
+    Private Sub btnJeterLesDes_Click(sender As Object, e As EventArgs) Handles btnJeterLesDes.Click
+        '**************************************************************************************************
+        '*Procédure : btnJeterLesDes_Click
+        '*But : Initialiser une valeur aléatoire aux deux dés, afficher les images correspondantes et mettre 
+        '*      à jour le nombre de points.
+        '**************************************************************************************************
+
+        ' Déclarer les variables
+        Dim intMise As Integer
+        Dim intPoints As Integer
+        Const intMin = 0
+
+        ' Choisir aléatoirement la valeur des deux dés (de 1 à 6)
+        Randomize()
+
+        Dim intDeUn As Integer = Int(6 * Rnd()) + 1
+        Dim intDeDeux As Integer = Int(6 * Rnd()) + 1
+
+        'Afficher l'image correspondante aux valeurs des deux dés
+        Select Case intDeUn
+            Case 1
+                Me.picDeUn.Image = My.Resources.Un
+            Case 2
+                Me.picDeUn.Image = My.Resources.Deux
+            Case 3
+                Me.picDeUn.Image = My.Resources.Trois
+            Case 4
+                Me.picDeUn.Image = My.Resources.Quatre
+            Case 5
+                Me.picDeUn.Image = My.Resources.Cinq
+            Case 6
+                Me.picDeUn.Image = My.Resources.Six
+        End Select
+
+        Select Case intDeDeux
+            Case 1
+                Me.picDeDeux.Image = My.Resources.Un
+            Case 2
+                Me.picDeDeux.Image = My.Resources.Deux
+            Case 3
+                Me.picDeDeux.Image = My.Resources.Trois
+            Case 4
+                Me.picDeDeux.Image = My.Resources.Quatre
+            Case 5
+                Me.picDeDeux.Image = My.Resources.Cinq
+            Case 6
+                Me.picDeDeux.Image = My.Resources.Six
+        End Select
+
+        'Définir le nombre de points disponibles et la mise
+        intPoints = Val(Me.lblPoints.Text)
+        intMise = Val(Me.txtMise.Text)
+
+        'Déterminer si le joueur a gagner ou s'il a perdu et ajouter ou soustraire les points de la banque.
+        If ValiderSaisie(intMise, intPoints, intMin) Then
+
+            If SommeDes(intDeUn, intDeDeux) Mod 2 <> 0 Then
+                'Si la somme des dés est impaire, ajouter le double des points misés à la banque.
+                intPoints += 2 * intMise
+                Me.lblPoints.Text = intPoints
+
+            Else
+                'Si la somme est paire, soustraire le nombre de points misés de la banque.
+                intPoints -= intMise
+                Me.lblPoints.Text = intPoints
+            End If
+
+        Else
+            'Message d'erreur si la mise est inférieur à zéro ou supérieur au nombre de points dans la banque.
+            MessageBox.Show("Le nombre de points doit être entre 0 et " & intPoints & ".")
+        End If
+
+        'Mettre à jour le nombre de points dans la banque.
+        Me.lblPoints.Text = intPoints
+
+    End Sub
+
+    Function SommeDes(ByVal intNombreUn As Integer, ByVal intNombreDeux As Integer) As Integer
+        '**************************************************************************************************
+        '*Fonction : SommeDes
+        '*But : Additionner la valeur des deux dés et retourner la sommme.
+        '**************************************************************************************************
+
+        Dim intSomme As Integer
+        intSomme = intNombreUn + intNombreDeux
+
+        Return intSomme
+
+    End Function
+
+    Function ValiderSaisie(ByVal intNombre As Integer, ByVal intMAX As Integer, ByVal intMIN As Integer) As Boolean
+        '**************************************************************************************************
+        '*Fonction : ValiderSaisie
+        '*But : Déterminer si la mise est acceptable. Une mise acceptable se situe entre zéro et le nombre
+        '       de points disponibles dans la banque.
+        '**************************************************************************************************
+
+        If (intNombre > intMAX) Or (intNombre < intMIN) Then
+            Return False
+        Else
+            Return True
+        End If
+
+    End Function
+End Class
